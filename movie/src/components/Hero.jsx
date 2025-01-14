b8af92fb90c962a87e725724ae95f269b0c6e069
setap <khuseyn693@gmail.com>
2025-01-15 01:45:04 +0400

    some

--- movie/src/components/Hero.jsx
+++ movie/src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
         <img src={Batman} alt="Batman" className="h-full w-full object-cover" />
         <div className="absolute bottom-0 w-full bg-gradient-to-b from-transparent to-gega-red  dark:to-blue-900">
           <div className="container pl-10 lg:pl-0">
-            <h3 className="text-red-200 dark:text-green-300 md:dark:text-green-200 tracking-wider group-hover:mb-1 duration-500 text-bold capitalize ">
+            <h3 className="text-red-200 dark:text-green-200 tracking-wider group-hover:mb-1 duration-500 text-bold capitalize ">
               romantic , drama , action , thriller , horror
             </h3>
-            <h1 className="text-4xl lg:text-6xl dark:text-blue-300 md:dark:text-blue-200 group-hover:mb-1 duration-500">
+            <h1 className="text-3xl lg:text-5xl dark:text-blue-200 group-hover:mb-1 duration-500">
               Join Us
             </h1>
-            <p className="my-6 text-sky-200 group-hover:mb-4 duration-500 w-full lg:w-3/4 text-sm lg:text-base">
+            <p className="hidden md:flex mt-3 mb-8 text-sky-200  group-hover:mb-4 duration-500 w-3/4 text-base">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat at
               cumque maiores non consequatur veniam nesciunt mollitia! Sapiente
               culpa deleniti quis quam nam voluptas id, accusamus iure quisquam,
@@ -22,6 +22,13 @@ const Hero = () => {
               ipsa non? Lorem ipsum dolor sit, amet consectetur adipisicing
               elit. Sed hic culpa incidunt eveniet, reiciendis aperiam nihil
               nulla veniam maxime mollitia expedita natus cupiditate accusamus
+              voluptas quasi dolore soluta consequuntur quaerat!
+            </p>
+
+            <p className="flex md:hidden mt-3 mb-5  pr-[6px]  text-sky-200  group-hover:mb-3 duration-500 w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat at
+              cumque maiores non consequatur veniam nesciunt mollitia! Sapiente
+              culpa deleniti quis quam nam voluptas id, accusamus iure quisquam,
               voluptas quasi dolore soluta consequuntur quaerat!
             </p>
             <div className="flex space-x-8 opacity-0 group-hover:opacity-100 group-hover:mb-10 lg:group-hover:mb-14 duration-1000"></div>
