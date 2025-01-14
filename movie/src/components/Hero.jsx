cc74e0fbb4a6c9ff58eaf2feb772260a0e156f36
setap <khuseyn693@gmail.com>
2025-01-15 01:16:05 +0400

    green

--- movie/src/components/Hero.jsx
+++ movie/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <img src={Batman} alt="Batman" className="h-full w-full object-cover" />
         <div className="absolute bottom-0 w-full bg-gradient-to-b from-transparent to-gega-red  dark:to-blue-900">
           <div className="container pl-10 lg:pl-0">
-            <h3 className="text-red-200 dark:text-green-300 tracking-wider group-hover:mb-1 duration-500 text-semibold capitalize ">
+            <h3 className="text-red-200 dark:text-green-200 tracking-wider group-hover:mb-1 duration-500 text-semibold capitalize ">
               action , drama , thriller , horror
             </h3>
             <h1 className="text-4xl lg:text-6xl text-blue-200 group-hover:mb-1 duration-500">
