21ccdde9d4baa12f6670453ae63281f0246dc74f
setap <khuseyn693@gmail.com>
2025-01-15 12:38:56 +0400

    rose

--- movie/src/components/Hero.jsx
+++ movie/src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
         <img src={Batman} alt="Batman" className="h-full w-full object-cover" />
         <div className="absolute bottom-0 w-full bg-gradient-to-b from-transparent to-gega-red  dark:to-blue-900">
           <div className="container pl-10 lg:pl-0">
-            <h3 className="text-red-200 dark:text-green-200 tracking-wider group-hover:mb-1 duration-500 text-bold capitalize ">
+            <h3 className="text-blue-100 dark:text-green-200 tracking-wider group-hover:mb-1 duration-500 text-bold capitalize ">
               romantic , drama , action , thriller , horror
             </h3>
             <h1 className="text-3xl lg:text-5xl text-rose-200 dark:text-blue-200 group-hover:mb-1 duration-500">
               Join Us
             </h1>
-            <p className="hidden md:flex mt-3 mb-8 text-pink-100 dark:text-sky-200  group-hover:mb-4 duration-500 w-3/4 text-base">
+            <p className="hidden md:flex mt-3 mb-8 text-rose-200 dark:text-sky-200  group-hover:mb-4 duration-500 w-3/4 text-base">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat at
               cumque maiores non consequatur veniam nesciunt mollitia! Sapiente
               culpa deleniti quis quam nam voluptas id, accusamus iure quisquam,
@@ -25,7 +25,7 @@ const Hero = () => {
               voluptas quasi dolore soluta consequuntur quaerat!
             </p>
 
-            <p className="flex md:hidden mt-3 mb-5  pr-[6px]  text-red-200 dark:text-sky-200  group-hover:mb-3 duration-500 w-full text-sm">
+            <p className="flex md:hidden mt-3 mb-5  pr-[6px]  text-rose-200 dark:text-sky-200  group-hover:mb-3 duration-500 w-full text-sm">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat at
               cumque maiores non consequatur veniam nesciunt mollitia! Sapiente
               culpa deleniti quis quam nam voluptas id, accusamus iure quisquam,
